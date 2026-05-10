4da7aefc10b654470caf8047b74d321cda145132
temuriee <critstoggle@gmail.com>
2026-05-10 12:32:35 -0700

    finish project
    fixed toaster
    added seo tags

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,12 @@
 // app/contact/page.tsx
 import { ContactForm } from "@/features/contact/components/ContactForm";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact Us",
+  description:
+    "Get in touch with Luminae. Tell us about your concerns and we will get back to you as soon as possible.",
+};
 
 export default function ContactPage() {
   return (
---
+++ app/providers.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { Toaster } from "react-hot-toast";
+
+export function Providers() {
+  return <Toaster position="bottom-right" />;
+}
